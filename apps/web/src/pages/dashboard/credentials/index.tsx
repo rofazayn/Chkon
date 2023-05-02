5ac301e7f84c2b8b7d4a2abc3a6bd50616401111
Abderraouf Zine <rofazayn@gmail.com>
2023-05-02 18:16:16 +0100

    chore(web): setup ui for user credentials list

--- apps/web/src/pages/dashboard/credentials/index.tsx
+++ apps/web/src/pages/dashboard/credentials/index.tsx
@@ -1,10 +1,118 @@
-import { Text } from '@mantine/core'
+import {
+  Alert,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Loader,
+  Text,
+  TextInput,
+} from '@mantine/core'
 import DashboardLayout from '../../../components/_layouts/dashboard-layout'
+import { IconSearch, IconSeeding, IconWorldPlus } from '@tabler/icons-react'
+import Link from 'next/link'
+import useUser from '../../../hooks/useUser'
+import { useCredentialsQuery } from '../../../generated/graphql'
 
 const VerifiableCredentialsHome = () => {
+  const { user } = useUser()
+  const consentedCredentialsQuery = useCredentialsQuery({
+    variables: {
+      where: {
+        userId: { equals: (user?.id as string) || undefined },
+        holderConsent: { equals: true },
+        issuerConsent: { equals: true },
+      },
+    },
+    fetchPolicy: 'network-only',
+    pollInterval: 5000,
+  })
+
+  const unconsentedCredentialsQuery = useCredentialsQuery({
+    variables: {
+      where: {
+        userId: { equals: (user?.id as string) || undefined },
+        holderConsent: { equals: false },
+        issuerConsent: { equals: true },
+      },
+    },
+    fetchPolicy: 'network-only',
+    pollInterval: 5000,
+  })
+
+  console.log(consentedCredentialsQuery.data)
+  console.log(unconsentedCredentialsQuery.data)
   return (
     <DashboardLayout>
-      <Text>Verifiable Credentials</Text>
+      {user ? (
+        <>
+          <Flex
+            sx={{
+              width: '100%',
+              flexDirection: 'row',
+              alignItems: 'flex-end',
+              justifyContent: 'space-between',
+              gap: 16,
+            }}
+          >
+            <Box sx={{ width: '100%' }}>
+              <Text weight='bold' size='lg'>
+                {user?.name}&apos; credentials
+              </Text>
+              <Text color='dimmed' mt={-3}>
+                Here are credentials that have been issued to you
+              </Text>
+              <Box mt={8} sx={{ maxWidth: 520 }}>
+                <TextInput
+                  sx={{ width: '100%' }}
+                  icon={<IconSearch size={16} style={{ opacity: 0.75 }} />}
+                  variant='filled'
+                  placeholder='Search credentials'
+                />
+              </Box>
+            </Box>
+
+            <Box>
+              <Button
+                component={Link}
+                href='/dashboard/issuers/'
+                leftIcon={<IconSeeding size={18} />}
+                variant='light'
+                color='gray'
+              >
+                Request another credential
+              </Button>
+            </Box>
+          </Flex>
+          <Divider variant='dashed' my={16} />
+          <Box sx={{ width: '100%' }}>
+            <Box>
+              <Text weight='bold' size='md'>
+                Credentials you consent to
+              </Text>
+              <Text color='dimmed' mt={-3}>
+                These are the credentials that you already accepted to be yours
+              </Text>
+            </Box>
+          </Box>
+          <Divider variant='dashed' my={16} />
+          <Box sx={{ width: '100%' }}>
+            <Box>
+              <Text weight='bold' size='md'>
+                Credentials awaiting consent
+              </Text>
+              <Text color='dimmed' mt={-3}>
+                Here are the credentials awaiting your consent
+              </Text>
+            </Box>
+          </Box>
+        </>
+      ) : (
+        <Loader size='sm' />
+        // <Alert color='red'>
+        //   There was something wrong while trying to fetch your credentials
+        // </Alert>
+      )}
     </DashboardLayout>
   )
 }
--- apps/web/src/pages/dashboard/organizations/[orgId]/requests/[reqId]/handle.tsx
+++ apps/web/src/pages/dashboard/organizations/[orgId]/requests/[reqId]/handle.tsx
@@ -100,6 +100,7 @@ const OrgCredentialRequestHandle = ({ children }: { children: ReactNode }) => {
 
   async function handleIssueCredential() {
     try {
+      setLoading(true)
       const res = await createOneCredentialMutation({
         variables: {
           data: {
@@ -139,6 +140,7 @@ const OrgCredentialRequestHandle = ({ children }: { children: ReactNode }) => {
           },
         })
         if (updateReq.data) {
+          setLoading(false)
           notifications.show({
             title: 'Success!',
             message: `You have successfully issued a credential of type <${credTypeData?.credentialType?.typename}> to the target user`,
@@ -146,8 +148,10 @@ const OrgCredentialRequestHandle = ({ children }: { children: ReactNode }) => {
             color: 'violet',
             autoClose: 5000,
           })
+
           router.push(`/dashboard/organizations/${orgId}/requests/pending`)
         } else {
+          setLoading(false)
           notifications.show({
             title: 'Request update filled',
             message:
